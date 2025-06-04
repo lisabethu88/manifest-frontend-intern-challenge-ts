71e3fdeccd9e47955236893ab942c67572a297b4
Lisa Utsett <lisabethutsett@gmail.com>
2025-06-04 15:27:17 -0400

    Add placeholder Button component and use it in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Button from "./Button";
+
 function App() {
   return (
     <>
-     <h1>Manifest Frontend Intern Code challenge 2025</h1>
-     <p>Show off your components here</p>
+      <h1>Manifest Frontend Intern Code challenge 2025</h1>
+      <p>Show off your components here</p>
+      <Button />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
