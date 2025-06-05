72355cce15ce2c55c2edfd36be02ce1f061b2fc5
Lisa Utsett <lisabethutsett@gmail.com>
2025-06-05 17:28:09 -0400

    Refactor Button component

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
+import "./Button.css";
+interface ButtonProps {
+  children?: React.ReactNode;
+  variant?: "primary" | "secondary" | "text";
+  device?: "mobile" | "desktop";
+  disabled?: boolean;
+  inverted?: boolean;
 }
 
-export default Button
+const Button = ({
+  variant = "primary",
+  device = "desktop",
+  disabled = false,
+  inverted = false,
+  children = "",
+}: ButtonProps) => {
+  let classes = "";
+  if (inverted) {
+    classes += "btn-inverted";
+  }
+  classes += " btn-" + variant;
+  classes += " btn-" + device;
+
+  return (
+    <button disabled={disabled} className={classes}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
